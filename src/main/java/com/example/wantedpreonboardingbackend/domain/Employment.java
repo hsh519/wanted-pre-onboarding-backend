80e114df06b1b0a859ae13f2f647d7aeb3b7bceb
hsh_519 <suk000519@naver.com>
2023-10-17 12:30:07 +0900

    Test : 채용공고 수정 테스트
    채용공고 수정 기능 테스트

--- src/main/java/com/example/wantedpreonboardingbackend/domain/Employment.java
+++ src/main/java/com/example/wantedpreonboardingbackend/domain/Employment.java
@@ -1,11 +1,14 @@
 package com.example.wantedpreonboardingbackend.domain;
 
 import com.example.wantedpreonboardingbackend.dto.EmploymentDto;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter
 @NoArgsConstructor
 public class Employment {
 
--- src/test/java/com/example/wantedpreonboardingbackend/domain/EmploymentTest.java
+++ src/test/java/com/example/wantedpreonboardingbackend/domain/EmploymentTest.java
@@ -34,4 +34,32 @@ class EmploymentTest {
         Assertions.assertThat(size).isEqualTo(1);
     }
 
+
+    @Test
+    void 채용공고_수정() {
+        // 회사와 채용공고 등록
+        Company company = new Company("원티드랩", "한국", "서울");
+        companyRepository.save(company);
+        Employment employment = new Employment(
+                company,
+                "백엔드 주니어 개발자",
+                1000000,
+                "원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..",
+                "Java"
+        );
+        employmentRepository.save(employment);
+
+        // 채용공고를 가져와 수정했을 때
+        Employment findEmployment = employmentRepository.findById(1L).get();
+        findEmployment.setMoney(150000);
+        findEmployment.setContent("원티드랩에서 백엔드 주니어 개발자를 \'적극\' 채용합니다. 자격요건은..");
+        findEmployment.setSkill("Spring");
+        employmentRepository.save(findEmployment);
+
+        // 채용공고 수정이 성공했는지
+        Employment result = employmentRepository.findById(1L).get();
+        Assertions.assertThat(result.getMoney()).isEqualTo(150000);
+        Assertions.assertThat(result.getContent()).isEqualTo("원티드랩에서 백엔드 주니어 개발자를 \'적극\' 채용합니다. 자격요건은..");
+        Assertions.assertThat(result.getSkill()).isEqualTo("Spring");
+    }
 }
