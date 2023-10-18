7f83b666bc1d8689c30d17da48c1cd745c833e53
hsh_519 <suk000519@naver.com>
2023-10-18 11:19:37 +0900

    Fix: Syntax 에러 수정
    바뀌지 않은 EmploymentReadDto 타입을 EmploymentReadListDto 타입으로 변경하면서 Syntax 에러 수정

--- src/main/java/com/example/wantedpreonboardingbackend/repository/EmploymentRepository.java
+++ src/main/java/com/example/wantedpreonboardingbackend/repository/EmploymentRepository.java
@@ -2,6 +2,7 @@ package com.example.wantedpreonboardingbackend.repository;
 
 import com.example.wantedpreonboardingbackend.domain.Employment;
 import com.example.wantedpreonboardingbackend.dto.EmploymentReadListDto;
+import com.example.wantedpreonboardingbackend.dto.EmploymentReadOneDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface EmploymentRepository extends JpaRepository<Employment, Long> {
-    @Query("select new com.example.wantedpreonboardingbackend.dto.EmploymentReadDto(e.id, c.name, c.country, c.region, e.position, e.money, e.skill) from Employment e " +
+    @Query("select new com.example.wantedpreonboardingbackend.dto.EmploymentReadListDto(e.id, c.name, c.country, c.region, e.position, e.money, e.skill) from Employment e " +
             "left join Company c on e.company.id = c.id where " +
             "e.position like %:keyword% or " +
             "e.skill like %:keyword% or " +
@@ -18,9 +19,7 @@ public interface EmploymentRepository extends JpaRepository<Employment, Long> {
             "or c.region like %:keyword%")
     List<EmploymentReadListDto> findByKeyword(@Param("keyword") String keyword);
 
-    @Query("select new com.example.wantedpreonboardingbackend.dto.EmploymentReadDto(e.id, c.name, c.country, c.region, e.position, e.money, e.skill) from Employment e " +
+    @Query("select new com.example.wantedpreonboardingbackend.dto.EmploymentReadListDto(e.id, c.name, c.country, c.region, e.position, e.money, e.skill) from Employment e " +
             "left join Company c on e.company.id = c.id")
     List<EmploymentReadListDto> findAllEmployment();
-
-
 }
